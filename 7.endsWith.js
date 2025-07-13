d861b35a12791edb1fe493964920b16cd1d045a7
shadid06 <hasnainbauet@gmail.com>
2025-07-13 17:21:41 +0600

    endswith and indexof

---
+++ 7.endsWith.js
@@ -0,0 +1,8 @@
+
+// endsWith() method checks whether a string ends with specified string/characters
+// endsWith() method returns true if the string ends with the characters, otherwise it returns false
+// endsWith() method is case sensitive
+
+console.log("report.pdf".endsWith(".pdf")); // true
+
+// Project Use Case: Restrict file uploads to .jpg, .png, .pdf.
---
+++ 8.indexOf.js
@@ -0,0 +1,8 @@
+
+// The indexOf() method returns the index of (the position of) the first occurrence of a specified text in a string:
+
+let text = "Please locate where 'locate' occurs!";
+let index = text.indexOf("locate");
+console.log(index); // 7
+
+// Project Use Case: Highlight matched text in search.
