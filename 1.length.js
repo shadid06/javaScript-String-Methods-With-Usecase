756d9422cd8dbb4186e0069469e6b955e491048c
shadid06 <hasnainbauet@gmail.com>
2025-07-13 15:57:16 +0600

    length

---
+++ 1.length.js
@@ -0,0 +1,9 @@
+// length property count the number of characters in a string
+
+// Real life: Count how many letters are in your name.
+
+const str = "Hello World";
+const length=str.length;
+console.log(length); // 11
+
+// Project Use Case: Limit input field characters (e.g., max 200 characters).
