0ea056f8d8c7e31cb82044497cbca3bb6eba8cfb
shadid06 <hasnainbauet@gmail.com>
2025-07-13 16:01:38 +0600

    to upper case

---
+++ 2.toUpperCase.js
@@ -0,0 +1,9 @@
+// toUpperCase() – Make everything uppercase
+
+// Real life: SHOUTING in all caps!
+
+const string = 'Hello World';
+const result = string.toUpperCase();
+console.log(result); // "HELLO WORLD"
+
+// Project Use Case: Convert country codes like "us" to "US".
