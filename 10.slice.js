73c64e24682d0cd14cec768603edb8cd0d3fe341
shadid06 <hasnainbauet@gmail.com>
2025-07-16 15:43:17 +0600

    till slice

---
+++ 10.slice.js
@@ -0,0 +1,5 @@
+// slice method can be used to extract a part of a string
+
+const str = "Hello World!";
+const res = str.slice(0, 5); // Extracts characters from index 0 to 4
+console.log(res); // Output: "Hello"
---
+++ 9.lastIndexOf.js
@@ -0,0 +1,9 @@
+
+// The lastIndexOf() method returns the index of the last occurrence of a specified value in a string.
+// The lastIndexOf() method returns -1 if the value is not found.
+// The lastIndexOf() method searches the string from the end to the beginning, stopping after the first match is found.
+// The lastIndexOf() method is case sensitive.
+
+console.log("Hi. Hello. Bye.".lastIndexOf(".")); // 14
+
+// Project Use Case: Extract filename before extension.
